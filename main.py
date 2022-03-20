b0eaff22a84e22946fda2a911f5733c72b7c2da7
Alexey Snytnikov <snytav@gmail.com>
2022-03-21 00:37:59 +0700

    delete spaces 2

--- main.py
+++ main.py
@@ -150,66 +150,6 @@ def Simplex(A, b, c):
     cbT, cnT, cindx, nonbasicSize = init_simplex(A)
     # run core simplex method until reach the optimal solution
     return core_simplex( cindx, nonbasicSize, A, cnT, cbT)
-    # while True:
-    #
-    #     # keep track of current indices of basic and non-basic variables
-    #     cbIndx = cindx[nonbasicSize:]
-    #     cnIndx = cindx[:nonbasicSize]
-    #
-    #     # basis matrix
-    #     B = A[:, cbIndx]
-    #     Binv = np.linalg.inv(B)
-    #
-    #     # nonbasic variable matrix
-    #     N = A[:, cnIndx]
-    #
-    #     # bHat, the values of the basic variables
-    #     # recall that at the start the basic variables are the slack variables, and
-    #     # have values equal the vector b (as primary variables are set to 0 at the start)
-    #     bHat = Binv @ b
-    #     yT = cbT @ Binv
-    #
-    #     # use to check for optimality, determine variable to enter basis
-    #     cnHat = cnT - (yT @ N)
-    #
-    #     # find indx of minimum value of cnhat, this is the variable to enter the basis
-    #     cnMinIndx = np.argmin(cnHat)
-    #
-    #     # break out of loop, returning values if all values of cnhat are above 0
-    #     if (all(i >= 0 for i in cnHat)):
-    #         # use cbIndx to get index values of variables in bHat, and the corresponding index
-    #         # values in bHat are the final solution values for each of the corresponding variables
-    #         # ie value 0 in dbIndx corresponds with first variable, so whatever the index for the 0 is
-    #         # is the index in bHat that has the solution value for that variable.
-    #         return cbT, cbIndx, cnT, cnIndx, bHat, cnHat
-    #
-    #     # this is the index for the column of coeffs in a for the given variable
-    #     indx = cindx[cnMinIndx]
-    #
-    #     Ahat = Binv @ A[:, indx]
-    #
-    #     ratios = get_non_basic_ratios(Ahat,bHat)
-    #     # now we want to iterate through Ahat and bHat and pick the minimum ratios
-    #     # only take ratios of variables with Ahat_i values greater than 0
-    #     # pick smallest ratio to get variable that will become nonbasic.
-    #     # ratios = []
-    #     # for i in range(0, len(bHat)):
-    #     #     Aval = Ahat[i]
-    #     #     Bval = bHat[i]
-    #     #
-    #     #     # don't look at ratios with val less then or eqaul to 0, append to keep index
-    #     #     if (Aval <= 0):
-    #     #         ratios.append(10000000)
-    #     #         continue
-    #     #     ratios.append(Bval / Aval)
-    #
-    #     ratioMinIndx = np.argmin(ratios)
-    #
-    #     # switch basic and nonbasic variables using the indices.
-    #     cnT[cnMinIndx], cbT[ratioMinIndx] = cbT[ratioMinIndx], cnT[cnMinIndx]
-    #     # switch global index tracker indices
-    #     cindx[cnMinIndx], cindx[ratioMinIndx + nonbasicSize] = cindx[ratioMinIndx + nonbasicSize], cindx[cnMinIndx]
-    #     # now repeat the loop
 
 # Example Input
 A = np.array([[-2, 1, 1, 0, 0],
